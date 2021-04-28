c4085b795fc7d4e5006e99d58ef76b5e45e44e68
rooparsh <rooparshkalia@gmail.com>
2021-04-28 09:04:21 +0530

    insertion sort done

--- src/main/kotlin/InsertionSort.kt
+++ src/main/kotlin/InsertionSort.kt
@@ -9,5 +9,15 @@ fun main() {
 }
 
 fun insertionSort(array: MutableList<Int>): List<Int> {
+    if (array.isEmpty()) return array
+    for (i in 0 until array.size) {
+        var j = i
+        while (j > 0 && array[j - 1] > array[j]) {
+            array[j - 1] = array[j - 1] - array[j]
+            array[j] = array[j - 1] + array[j]
+            array[j - 1] = array[j] - array[j - 1]
+            j--
+        }
+    }
     return array
 }
