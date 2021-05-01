b00b10cd9b1c8ea2a3e4e3a61e10a8cb17d2a032
rooparsh <rooparshkalia@gmail.com>
2021-05-01 22:01:17 +0530

    non constructible done

--- src/main/kotlin/NonConstructibleChange.kt
+++ src/main/kotlin/NonConstructibleChange.kt
@@ -9,6 +9,13 @@ fun main() {
 }
 
 fun nonConstructibleChange(coins: MutableList<Int>): Int {
-    // Write your code here.
-    return -1
+    coins.sort()
+    var currentChangeCreated = 0
+    for (coin in coins) {
+        if (coin > currentChangeCreated + 1) {
+            return currentChangeCreated + 1
+        }
+        currentChangeCreated += coin
+    }
+    return currentChangeCreated + 1
 }
